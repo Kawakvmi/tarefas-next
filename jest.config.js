4dc8a0dd394415479e68bed9a103fc268c4b37b8
Dereck <dereckkawakami@gmail.com>
2026-02-16 09:33:36 -0300

    fix: remove nested project and restore jest/types

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
 ﻿const nextJest = require("next/jest");
 
-const createJestConfig = nextJest({
-  dir: "./",
-});
+const createJestConfig = nextJest({ dir: "./" });
 
 const customJestConfig = {
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@/(.*)$": "<rootDir>/src/$1"
   },
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/.next/"
+  ]
 };
 
 module.exports = createJestConfig(customJestConfig);
--- tarefas-next/todo-tests/src/app/hooks/useContadorDeTarefas.ts
+++
@@ -1,7 +0,0 @@
-import { Tarefa } from "../data/tarefas";
-import { useMemo } from "react";
-
-export default function useContadorDeTarefas(tarefas: Tarefa[]) {
-  const quantidade = useMemo(() => tarefas.length, [tarefas]);
-  return quantidade;
-}
